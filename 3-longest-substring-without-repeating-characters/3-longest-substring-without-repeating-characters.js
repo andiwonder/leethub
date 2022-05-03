6aafa324c9f999fb99d909c1f56bd43fe865a412
Swapnil Kasliwal <skasliwal12@gmail.com>
2022-05-03 08:04:34 -0400

    Time: 86 ms (92.04%), Space: 44.6 MB (97.10%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -1,19 +1,18 @@
 var lengthOfLongestSubstring = function(s) {
-    let max_len = 0;
-    let curr = 0;
-    let hash = {}; 
-    if(s.length < 2) {
-        return s.length;
+  var k = 0;
+  var maxLength = 0;
+
+  for(i = 0; i < s.length; i++) {
+    for (j = k; j < i; j++) {
+      if (s[i] === s[j]) {
+        k = j + 1;
+        break;
+      }
     }
-    for(let i = 0; i < s.length;  i++) {
-        let char = s[i];
-        if(hash[char] == null) {
-            curr += 1;
-        } else {
-            curr = Math.min(i - hash[char], curr + 1);
-        }
-        max_len = Math.max(max_len, curr);
-        hash[char] = i; //save the index
+    if (i - k + 1 > maxLength) {
+      maxLength = i - k + 1;
     }
-    return max_len;
+  }
+
+  return maxLength;
 };
